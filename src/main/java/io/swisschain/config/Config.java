00a4f440495bdfc7bfbe282f9305642e3eff5638
Alexander Ushakov <alexander.n.ushakov@gmail.com>
2020-10-29 02:37:17 +0300

    Tasks period settings.

--- src/main/java/io/swisschain/config/Config.java
+++ src/main/java/io/swisschain/config/Config.java
@@ -1,8 +1,11 @@
 package io.swisschain.config;
 
+import io.swisschain.config.tasks.TasksConfig;
+
 public class Config {
   public String instanceName;
   public HsmConfig hsmConfig;
   public DbConfig db;
   public VaultApiConfig vaultApi;
+  public TasksConfig tasks;
 }
